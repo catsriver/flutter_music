34b05c2879a1615387430468fea037012558f215
猫河 <catsriver@163.com>
2022-08-31 04:02:34 +0800

    refactor: 添加SearchBox、CustomAppBar widget

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import '../../styles/styles.dart';
+import '../../widgets/common/search_box.dart';
+import '../../widgets/custom_app_bar/custom_app_bar.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -10,53 +10,16 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const FaIcon(FontAwesomeIcons.bars),
+      appBar: CustomAppBar(
+        middle: SearchBox(
+          title: '天外来物',
+          subTitle: '薛之谦',
+          press: () {},
+        ),
+        trailing: IconButton(
+          icon: const FaIcon(FontAwesomeIcons.microphoneLines),
           onPressed: () {},
         ),
-        titleSpacing: 0,
-        title: Container(
-          height: 54.h,
-          decoration: ShapeDecoration(
-            color: const Color(0xFFFEFEFE),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(54.h),
-            ),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              FaIcon(
-                FontAwesomeIcons.magnifyingGlass,
-                color: Colours.fontColor2,
-                size: Dimens.fontSp22,
-              ),
-              SizedBox(width: Dimens.gapWDp5),
-              Text(
-                '天外来物',
-                style: TextStyle(
-                  fontSize: Dimens.fontSp22,
-                  color: Colours.fontColor2,
-                ),
-              ),
-              SizedBox(width: Dimens.gapWDp5),
-              Text(
-                '薛之谦',
-                style: TextStyle(
-                  fontSize: Dimens.fontSp22,
-                  color: Colours.fontColor3,
-                ),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const FaIcon(FontAwesomeIcons.microphoneLines),
-          )
-        ],
       ),
       body: const Center(
         child: Text('发现'),
---
+++ lib/widgets/custom_app_bar/custom_app_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    Key? key,
+    this.leading,
+    required this.middle,
+    required this.trailing,
+  }) : super(key: key);
+
+  final Widget? leading;
+  final Widget middle;
+  final Widget trailing;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: leading ??
+          IconButton(
+            icon: const FaIcon(FontAwesomeIcons.bars),
+            onPressed: () {},
+          ),
+      titleSpacing: 0,
+      title: middle,
+      actions: [trailing],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
