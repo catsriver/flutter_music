399a9f0667862beb88f9ff8663ae792ff98fde29
猫河 <catsriver@163.com>
2022-08-31 01:50:14 +0800

    style(theme): 添加主题数据

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import './styles/styles.dart';
+
 void main() {
   runApp(const App());
 }
@@ -18,6 +20,26 @@ class App extends StatelessWidget {
         return MaterialApp(
           title: '网易云音乐',
           debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+              elevation: 0,
+              centerTitle: true,
+              backgroundColor: Colours.secondaryBgColor,
+              foregroundColor: Colours.fontColor1,
+            ),
+            bottomNavigationBarTheme: BottomNavigationBarThemeData(
+              elevation: 0,
+              selectedItemColor: Colours.accentColor,
+              unselectedItemColor: Colours.fontColor3,
+              selectedLabelStyle: TextStyle(
+                fontSize: Dimens.fontSp14,
+              ),
+              unselectedLabelStyle: TextStyle(
+                fontSize: Dimens.fontSp14,
+              ),
+            ),
+            scaffoldBackgroundColor: Colours.secondaryBgColor,
+          ),
           home: Scaffold(
             appBar: AppBar(
               title: const Text('网易云音乐'),
