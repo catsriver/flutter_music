2aee81f784bed062a2c551feaad17f23a359ed2b
猫河 <catsriver@163.com>
2022-09-02 08:47:59 +0800

    feat(banner): 动态渲染banner

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,11 +1,14 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_music/providers/banner_provider.dart';
+import 'package:flutter_music/screens/search/widgets/banner_item.dart';
 import 'package:flutter_music/styles/styles.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../widgets/common/search_box.dart';
 import '../../widgets/custom_app_bar/custom_app_bar.dart';
-import './widgets/banner_item.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -28,19 +31,36 @@ class SearchScreen extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: Dimens.gapWDp24),
         children: [
           // Banner
-          Container(
-            height: 190.h,
-            decoration: BoxDecoration(
-              color: Colors.indigo,
-              borderRadius: BorderRadius.circular(Dimens.radiusH24 / 2),
-            ),
-            clipBehavior: Clip.hardEdge,
-            child: const BannerItem(
-              imgUrl:
-                  'http://p1.music.126.net/5OlcZm8j5tCwo2MjtagGgg==/109951167834116990.jpg',
-              typeTitle: '热歌推荐',
-              titleBgColor: Colors.red,
-            ),
+          Consumer(
+            builder: (_, ref, __) {
+              final banners = ref.watch(bannerProvider);
+              return Container(
+                height: 190.h,
+                decoration: BoxDecoration(
+                  color: Colors.indigo,
+                  borderRadius: BorderRadius.circular(Dimens.radiusH24 / 2),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: Swiper(
+                  autoplay: true,
+                  pagination: SwiperPagination(
+                    margin: EdgeInsets.only(bottom: Dimens.gapHDp10),
+                    builder: DotSwiperPaginationBuilder(
+                      size: 10.h,
+                      activeSize: 10.h,
+                      color: const Color(0xFFCCCCCC),
+                      activeColor: const Color(0xFFFCFCFC),
+                    ),
+                  ),
+                  itemCount: banners.length,
+                  itemBuilder: (_, index) => BannerItem(
+                    imgUrl: banners[index].imgUrl,
+                    typeTitle: banners[index].titleType,
+                    titleBgColor: banners[index].titleBgColor,
+                  ),
+                ),
+              );
+            },
           ),
         ],
       ),
